e14c6fe6c65d0989b2c332378af22a4f18c86634
Taiga Ueta <taiga.ueta@pwc.com>
2018-07-03 08:37:23 +0000

    helperファイルで共通のログイン判定用モジュール作成

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+  # ログイン中のユーザを取得
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  # ログイン判定用のモジュールの作成
+  def logged_in?
+    current_user.present?
+  end
 end
