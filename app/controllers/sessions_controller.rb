b6c2eb70cd603cb7543ee27ec1c249586136626e
Taiga Ueta <taiga.ueta@pwc.com>
2018-07-03 08:35:28 +0000

    ヘルパーモジュールを使ったコントローラファイルの更新

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,12 @@ class SessionsController < ApplicationController
   
   def create
     # 送信されたemailを元にUserデータテーブルからデータを検索
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase) #データテーブルのクラス確認!!!!!!!!!!!!!!!!!!!!!!!!!
     # パスワードが合っているか判定
     if user && user.authenticate(params[:session][:password])
       # ログイン成功した場合
+      session[:user_id] = user.id 
+      redirect_to user_path(user.id) #ルーティング確認!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     else
       # ログイン失敗した場合
       flash.now[:danger] = 'ログインに失敗しました'
