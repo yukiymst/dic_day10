c00087e770bf8799c64363173143387fb592a537
yuya miyamoto <yuya.miyamoto@pwc.com>
2018-07-03 09:29:39 +0000

    bootstrap_1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap/dist/js/bootstrap.min
 //= require rails-ujs
 //= require turbolinks
+//= require jquery/dist/jquery.js
 //= require_tree .
